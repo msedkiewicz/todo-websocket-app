52e68b5bf625f95b7ff02a545366684844563e63
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-31 19:58:41 +0100

    Configure socket.IO

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+const socket = require("socket.io");
+
+const tasks = [];
+
+const server = app.listen(8000, () => {
+  console.log("Server is running on Port:", 8000);
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+  res.status(404).send("404: Page not found");
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected with ID:", socket.id);
+  socket.emit('updateData', tasks);
+  socket.on("addTask", (task) => {
+    console.log("Task added");
+    tasks.push(task);
+    socket.broadcast.emit("addTask", task);
+  });
+  socket.on("removeTask", (task) => {
+    tasks.splice(tasks.indexOf(task), 1);
+    socket.broadcast.emit("removeTask", task);
+  });
 });
